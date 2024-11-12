5fa62be0a943553a537526cc8b603eee894ffd98
Ramazan-Z <Ramazan_zalilov@mail.ru>
2024-11-12 22:04:23 +0400

    CRUD for User

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
+from rest_framework import routers
+
 from users import views
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register("", views.UserViewSet, basename="users")
+
+urlpatterns = router.urls
