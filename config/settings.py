d53e1a2a5b2ab99a4cb03f5fbeb3ff66edb28469
Ramazan-Z <Ramazan_zalilov@mail.ru>
2024-11-21 10:59:52 +0400

    install djangorestframework-simplejwt

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -25,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Created apps
+    "rest_framework_simplejwt",
     "rest_framework",
     "django_filters",
     "materials",
@@ -108,13 +110,21 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Модель пользователя
 AUTH_USER_MODEL = "users.User"
 
-# Настройка REST
+# Настройка  REST_FRAMEWORK
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
+}
+
+# Настройки срока действия токенов
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users import views
 from users.apps import UsersConfig
@@ -7,8 +8,10 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet, basename="users")
+router.register("", views.UserViewSet, basename="users")
 
 urlpatterns = [
-    path("pyments/", views.PymentsList.as_view(), name="pyments-list"),
+    path("pyments/", views.PaymentsList.as_view(), name="pyments-list"),
+    path("token/", TokenObtainPairView.as_view(), name="token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + router.urls
